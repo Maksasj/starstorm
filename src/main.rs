ee1475854693dac17efe4e992008c76e737125f8
maksasj <radioboos@gmail.com>
2023-06-04 17:48:24 +0300

    window now not resizable

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use bevy::{
 use bevy_kira_audio::prelude::*;
 
 mod resources;
-use crate::components::death_scene_system::{despawn_death_scene_entities, self};
-use crate::resources::death_screen_background::spawn_death_screen_background_system;
 pub use crate::resources::{
     sprite_sheet::*,
     mouse_position::*,
@@ -73,11 +71,17 @@ fn main() {
             ).set(WindowPlugin {
                 primary_window: Some(Window {
                     title: "Starstorm".into(),
-                    resolution: (800., 600.).into(),
+                    resolution: (800.0, 600.0).into(),
                     present_mode: PresentMode::AutoVsync,
                     fit_canvas_to_parent: true,
                     resizable: false,
                     prevent_default_event_handling: false,
+                    resize_constraints: WindowResizeConstraints{
+                        min_width: 800.0,
+                        min_height: 600.0,
+                        max_width: 800.0,
+                        max_height: 600.0,
+                    },
                     ..default()
                 }),
                 ..default()
