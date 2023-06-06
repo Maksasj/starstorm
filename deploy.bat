b3aab9874073f8d6b9c0cd112776689da7801d4f
maksasj <radioboos@gmail.com>
2023-06-06 14:06:49 +0300

    web build

--- deploy.bat
+++ deploy.bat
@@ -17,11 +17,13 @@ echo !ESC![92mDeploying exe build!ESC![0m
 
 echo !ESC![91mDeleting previous web build!ESC![0m
     rmdir /s/q dist\web
+    rmdir /s/q web
 
 echo !ESC![93mBuilding exe build!ESC![0m
     cargo build --release --target wasm32-unknown-unknown
     wasm-bindgen --out-dir ./dist/web --target web ./target/wasm32-unknown-unknown/release/starstorm.wasm
 
 echo !ESC![92mDeploying web build!ESC![0m
-    xcopy /i/s/e/f .\web\ .\dist\web\
+    xcopy /i/s/e/f .\html\ .\dist\web\
     xcopy /i/s/e/f .\assets\ .\dist\web\assets
+    xcopy /i/s/e/f .\dist\web\ .\web\
